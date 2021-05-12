bb65f37da792a05201dff938773aac7aebc08b49
dfurdi <62663055+dfurdi@users.noreply.github.com>
2021-05-13 01:33:23 +0200

    Add files via upload

---
+++ entFramework/Form1.Designer.cs
@@ -0,0 +1,412 @@
+﻿
+namespace entFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(135, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 57);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "dodaj/izmjeni";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Proizvodac";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Model";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 15);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Boja";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 110);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 15);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "BrojKotaca";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 15);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "GodinaProizvodnje";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 193);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 21;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 149);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 23);
+            this.numericUpDown1.TabIndex = 25;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 222);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(117, 19);
+            this.checkBox1.TabIndex = 26;
+            this.checkBox1.Text = "PotamljenaStakla";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(289, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(931, 379);
+            this.listBox1.TabIndex = 27;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(12, 471);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 57);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "filtriraj po:";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(135, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 57);
+            this.button3.TabIndex = 29;
+            this.button3.Text = "pobrisi";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(166, 471);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(1054, 379);
+            this.listBox2.TabIndex = 30;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(19, 745);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(117, 19);
+            this.checkBox2.TabIndex = 41;
+            this.checkBox2.Text = "PotamljenaStakla";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(19, 672);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(100, 23);
+            this.numericUpDown2.TabIndex = 40;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 698);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 15);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "GodinaProizvodnje";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(19, 716);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 38;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 656);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 15);
+            this.label7.TabIndex = 37;
+            this.label7.Text = "BrojKotaca";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(19, 615);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 15);
+            this.label8.TabIndex = 36;
+            this.label8.Text = "Boja";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(19, 633);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 23);
+            this.textBox6.TabIndex = 35;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(19, 574);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 15);
+            this.label9.TabIndex = 34;
+            this.label9.Text = "Model";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(19, 592);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 23);
+            this.textBox7.TabIndex = 33;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(19, 531);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 15);
+            this.label10.TabIndex = 32;
+            this.label10.Text = "Proizvodac";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(19, 549);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 23);
+            this.textBox8.TabIndex = 31;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(135, 184);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 57);
+            this.button4.TabIndex = 42;
+            this.button4.Text = "pobrisi sve";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1232, 864);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "lol";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ entFramework/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace entFramework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dohvati();
+        }
+        private void dohvati()
+        {
+            listBox1.Items.Clear();
+
+            AutoContext db = new AutoContext();
+
+            foreach (var item in db.Automobili)
+            {
+                listBox1.Items.Add("ID : " + item.Id + " Proizvodac : " + item.Proizvodac + " Model - " + item.Model + " GOD - " + item.GodinaProizvodnje + " Boja - " + item.Boja + " Kotaci - " + item.BrojKotaca + " Stakla jesu potamljena - " + item.PotamljenaStakla);
+            }
+            listBox1.Items.Add("NEW...");
+        }
+
+        private void button1_Click(object sender, EventArgs e) //kreiraj
+        {
+            AutoContext db = new AutoContext();
+
+            if (listBox1.SelectedItem == "NEW..." || listBox1.SelectedItem == null || listBox1.SelectedItem == "")
+            {
+                if (textBox1.Text == null || textBox1.Text == "" || textBox2.Text == null || textBox2.Text == "" || textBox3.Text == null || textBox3.Text == "" || textBox5.Text == null || textBox5.Text == "")
+                {
+                    MessageBox.Show("cant be empty");
+                    return;
+                }
+                else
+                {
+                    int dasdasd = System.Convert.ToInt32(numericUpDown1.Value);
+
+                    db.Automobili.Add(new Automobil()
+                    {
+                        Proizvodac = textBox1.Text,
+                        Model = textBox2.Text,
+                        Boja = textBox3.Text,
+                        BrojKotaca = dasdasd,
+                        GodinaProizvodnje = textBox5.Text,
+                        PotamljenaStakla = checkBox1.Checked
+
+                    });
+                }
+
+                db.SaveChanges();
+                dohvati();
+            }
+            else
+            {
+                string[] words = listBox1.SelectedItem.ToString().Split(' ');
+
+                int id = System.Convert.ToInt32(words[2]);
+
+                var auto = db.Automobili.Find(id);
+
+                auto.Proizvodac = textBox1.Text;
+                auto.Model = textBox2.Text;
+                auto.Boja = textBox3.Text;
+                auto.BrojKotaca = System.Convert.ToInt32(numericUpDown1.Value);
+                auto.GodinaProizvodnje = textBox5.Text;
+                auto.PotamljenaStakla = checkBox1.Checked;
+
+
+                db.SaveChanges();
+                dohvati();
+            }
+            }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            AutoContext db = new AutoContext();
+            if (listBox1.SelectedItem != "NEW...")
+            {
+                string[] words = listBox1.SelectedItem.ToString().Split(' ');
+
+                int id = System.Convert.ToInt32(words[2]);
+
+                var auto = db.Automobili.Find(id);
+
+                textBox1.Text = auto.Proizvodac;
+                textBox2.Text = auto.Model;
+                textBox3.Text = auto.Boja;
+                numericUpDown1.Value = auto.BrojKotaca;
+                textBox5.Text = auto.GodinaProizvodnje;
+                checkBox1.Checked = auto.PotamljenaStakla;
+            }
+            else
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                numericUpDown1.Value = 4;
+                textBox5.Text = "";
+                checkBox1.Checked = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)//filter button
+        {
+            AutoContext db = new AutoContext();
+
+            listBox2.Items.Clear();
+
+            foreach (var item in db.Automobili)
+            {
+                if (item.Proizvodac.ToLower() == textBox8.Text.ToLower() || item.Model.ToLower() == textBox7.Text.ToLower() || item.Boja.ToLower() == textBox6.Text.ToLower() || item.GodinaProizvodnje == textBox4.Text)
+                {
+                    listBox2.Items.Add("Proizvodac : " + item.Proizvodac + " Model - " + item.Model + " GOD - " + item.GodinaProizvodnje + " Boja - " + item.Boja + " Kotaci - " + item.BrojKotaca + " Stakla jesu potamljena - " + item.PotamljenaStakla);
+                }
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            AutoContext db = new AutoContext();
+            foreach (var item in db.Automobili)
+            {
+                db.Automobili.Remove(item);
+            }
+            db.SaveChanges();
+            dohvati();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                AutoContext db = new AutoContext();
+
+                string[] words = listBox1.SelectedItem.ToString().Split(' ');
+
+                int id = System.Convert.ToInt32(words[2]);
+
+
+                var auto = db.Automobili.Find(id);
+                db.Automobili.Remove(auto);
+                db.SaveChanges();
+                dohvati();
+            }
+            else
+            {
+                MessageBox.Show("lol");
+            }
+        }
+    }
+    }
+
+    public class AutoContext : DbContext
+    {
+        public AutoContext()
+        {
+
+        }
+
+        // Entities        
+        public DbSet<Automobil> Automobili { get; set; }
+    }
+    public class Automobil
+    {
+    public int Id { get; set; }
+    public string Model { get; set; }
+        public string Proizvodac { get; set; }
+        public string GodinaProizvodnje { get; set; }
+        public string Boja { get; set; }
+        public bool PotamljenaStakla { get; set; }
+        public int BrojKotaca { get; set; }
+    }
+
+
+
+
+
+
+//string[] bojeArray = { "Crvena", "Plava", "Zelena", "Narancasta", "Bjela", "Crna", "Ljubicasta", "Zuta", "Tamno crvena", "Tamno plava"};
+//string[] godProizArray = { "2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012" };
+//string[] modelArray = { "S", "i8", "R8", "a45", "Vitara", "Wrx", "xc90", "Arteon", "911", "Aventador" };
+//string[] proizvodacArray = { "Tesla", "BMW", "Audi", "Mercedes", "Suzuki", "Subaru", "Volvo", "Volkswagen", "Ferrari", "Lamborghini" };
+//bool stakloRes = true;
+
+
+//for (int i = 0; i < 9; i++)
+//{
+//    Automobil a = new Automobil();
+//    Random r = new Random();
+
+
+
+
+//    int randInt = r.Next(1, 2);
+//    if (randInt == 1)
+//    {
+//        stakloRes = true;
+//    }
+//    if (randInt == 2)
+//    {
+//        stakloRes = false;
+//    }
+
+//    db.Automobili.Add(new Automobil() { Boja = bojeArray[i], BrojKotaca = 4, GodinaProizvodnje = godProizArray[i], Model = modelArray[i], Proizvodac = proizvodacArray[i], PotamljenaStakla = stakloRes });
+
